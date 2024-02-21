030cd3fa8a0cda300cf6398e6c5929818271e125
revinapermanasari <146794965+revinapermanasari@users.noreply.github.com>
2024-02-21 20:27:09 +0700

    Add files via upload

---
+++ Praktikum1/Program Pertama/variable_user.php
@@ -0,0 +1,11 @@
+<?php
+$Nama = "Revina Permanasari";
+$Umur = 22;
+$Berat = 44.0;
+
+echo 'Nama : ' . $Nama;
+echo '<br/>Umur : ' . $Umur.' Tahun';
+echo '<br/>Berat : '.$Berat.' Kg'; 
+
+echo '<br/>Hello ' . $Nama.' Apa kabar?';
+?> 
